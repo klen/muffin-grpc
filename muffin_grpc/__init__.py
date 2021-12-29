cb2e2dc73a9f0b7d69598379e7c9f9c6a8f29dee
Kirill Klenov <horneds@gmail.com>
2021-12-29 17:20:35 +0300

    Bump version: 0.3.4 → 0.3.5

--- muffin_grpc/__init__.py
+++ muffin_grpc/__init__.py
@@ -21,7 +21,7 @@ except ImportError:
 
 from .utils import _fix_imports, _generate_file, _is_newer, _parse_proto
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 
 INCLUDE = resource_filename("grpc_tools", "_proto")
